fa352687358dba3555de7d71e14722ad68090ebf
Bonnie <ying.bao@stmartin.edu>
2024-04-04 13:48:32 -0700

    unitTesting

---
+++ program.java
@@ -0,0 +1,15 @@
+/**
+ * program
+ */
+public class program {
+
+    public static void main(String[] args) {
+        double outcome = add(2, 4);
+        System.out.println(outcome == 6);
+        
+    }
+
+    public static double add(double x, double y){
+        return x + y;
+    }
+}
